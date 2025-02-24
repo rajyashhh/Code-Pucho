09354cae4caaba530fcdca5b038b75998ae167cd
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-24 23:00:52 +0530

    changes done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,3 +9,4 @@ app.get('/',(req,res)=>{
 })
 app.use('/ai',aiRoutes)
 module.exports = app;
+module.export=app;
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -7,10 +7,15 @@ const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const prompt = "Code Review Tool";
 
 const result = await model.generateContent(prompt);
-console.log(result.response.text());
+c,onsole.log(result.response.text());
+,
+asyn,c function generateContent(prompt){
+   , const result = await model.generateContent(prompt);
+  ,  return result.response.text();
+},
+m,,odule.exports=generateContent;
 
-async function generateContent(prompt){
-    const result = await model.generateContent(prompt);
-    return result.response.text();
-}
-module.exports=generateContent;
+,nkjnjkn,njnji,,,,,,,,,,,n,
+<hulala></hulala>
+
+
